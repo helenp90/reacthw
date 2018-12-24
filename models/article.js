cead885648a82e15bc877f6d81f333ba3641be98
Helen Pitt <helenpitt1990@gmail.com>
2018-12-24 11:52:05 -0500

    update to get this site fixed

--- models/article.js
+++ models/article.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const url = require("url");
 const articleSchema = new Schema({
   title: { type: String, required: true },
   date: { type: Date, default: Date.now },
   ///Helen figure out how to set this up!! Ask Mark or Pat!
-  url: { type: URL, required: true },
+  url: { type: url, required: true },
   ///
   synopsis: String,
   
